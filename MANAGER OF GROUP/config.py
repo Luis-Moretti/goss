7447b17289d3d49f8bc5182480c79614092a7698
Luis-Moretti <ma4muh@gmail.com>
2025-03-18 21:48:50 +0300

    Delete MANAGER OF GROUP directory

--- MANAGER OF GROUP/config.py
+++
@@ -1,6 +0,0 @@
-from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
-from vk_api import VkApi
-
-session_vk = VkApi(token="vk1.a.EfNLHGOP1abrcu0hwYC1ZRKTwLyxL5C_1hoQ8pVvZE3AUQGLxRITe0HY6cZluqwYo7k4ple2qbWyXRucJRuKStlj02dw8zrYoLtxuV3x1vrL5kmaD8tjgy_ibUbjB51LGPF88Bk7Ci_5PK-fd6m3dEqP-YPNvTTlyCTpqsIV6YSOJay0OjZIxzjD9M5pFapY85mBxhZUSvs8SOynK8vmPQ")
-vk_api = session_vk.get_api()
-longpoll = VkBotLongPoll(session_vk, 229655124)
